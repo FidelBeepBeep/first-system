0c748721379e2df668f7a10898bf18dbda176889
FidelBeepBeep <fsalvijo@mail.com>
2025-01-01 17:25:23 +0100

    Sistema base creat amb Boilerplate System

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FIRST_SYSTEM = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+FIRST_SYSTEM.abilities = {
+  str: 'FIRST_SYSTEM.Ability.Str.long',
+  dex: 'FIRST_SYSTEM.Ability.Dex.long',
+  con: 'FIRST_SYSTEM.Ability.Con.long',
+  int: 'FIRST_SYSTEM.Ability.Int.long',
+  wis: 'FIRST_SYSTEM.Ability.Wis.long',
+  cha: 'FIRST_SYSTEM.Ability.Cha.long',
+};
+
+FIRST_SYSTEM.abilityAbbreviations = {
+  str: 'FIRST_SYSTEM.Ability.Str.abbr',
+  dex: 'FIRST_SYSTEM.Ability.Dex.abbr',
+  con: 'FIRST_SYSTEM.Ability.Con.abbr',
+  int: 'FIRST_SYSTEM.Ability.Int.abbr',
+  wis: 'FIRST_SYSTEM.Ability.Wis.abbr',
+  cha: 'FIRST_SYSTEM.Ability.Cha.abbr',
+};
